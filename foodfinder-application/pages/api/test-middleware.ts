8805b707f06eb47ba411e81b6dc70606ef94077d
Davy <103334758+Descode1@users.noreply.github.com>
2025-07-09 18:19:46 +0200

    feat: implement location service functions and update API handler to fetch all locations

--- foodfinder-application/pages/api/test-middleware.ts
+++ foodfinder-application/pages/api/test-middleware.ts
@@ -1,10 +1,10 @@
 import type { NextApiRequest,NextApiResponse } from "next";
 import dbConnect from "middleware/db-connect";
-import Locations from "mongoose/locations/model";
+import { findLocationAll } from "@/mongoose/locations/service";
 
 export default async function handler (req:NextApiRequest, res:NextApiResponse<any>) {
   await dbConnect();
-  const locations = await Locations.find({});
+  const locations = await findLocationAll();
   res.status(200).json(locations);
 }
 
