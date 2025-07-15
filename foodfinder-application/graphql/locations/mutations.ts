7a46c4c00ab58d1084c31b2ad5f9102563a9c245
Davy <103334758+Descode1@users.noreply.github.com>
2025-07-15 18:38:06 +0200

    refactor(graphql): improve code structure and formatting in GraphQL API handler

--- foodfinder-application/graphql/locations/mutations.ts
+++ foodfinder-application/graphql/locations/mutations.ts
@@ -1,15 +1,34 @@
 import { updateWishlist } from "@/mongoose/locations/service";
-
-interface UpdateWishlistInterface{
-    user_id: string;
-    location_id: string;
+import { authGuard } from "middleware/auth-guard";
+import { JWT } from "next-auth/jwt";
+interface UpdateWishlistInterface {
+  user_id: string;
+  location_id: string;
 }
-
-export const locationMutation = {
-    removeWishlist: async(_: any, param: UpdateWishlistInterface, context: {}) =>{
-        return await updateWishlist(param.user_id,param.location_id,"remove");
-    },
-    addWishlist: async(_: any, param: UpdateWishlistInterface, context: {}) =>{
-        return await updateWishlist(param.user_id, param.location_id,"add");
-    },
+interface contextInterface {
+  token: JWT;
+}
+export const locationMutations = {
+  removeWishlist: async (
+    _: any,
+    param: UpdateWishlistInterface,
+    context: contextInterface
+  ) => {
+    const guard = authGuard(param, context);
+    if (guard !== true) {
+      return guard;
+    }
+    return await updateWishlist(param.location_id, param.user_id, "remove");
+  },
+  addWishlist: async (
+    _: any,
+    param: UpdateWishlistInterface,
+    context: contextInterface
+  ) => {
+    const guard = authGuard(param, context);
+    if (guard !== true) {
+      return guard;
+    }
+    return await updateWishlist(param.location_id, param.user_id, "add");
+  },
 };
