0fef5ca3db6cbaea67fbc7c58facb7eddc42f6d1
Davy <103334758+Descode1@users.noreply.github.com>
2025-07-15 12:32:27 +0200

    feat(auth-element): implement AuthElement component with authentication handling and styling

--- foodfinder-application/components/header/index.tsx
+++ foodfinder-application/components/header/index.tsx
@@ -1,12 +1,14 @@
 import { JSX } from "react"
 import styles from "./index.module.css"
 import Logo from "./logo"
+import AuthElement from "./auth-element";
 
 const Header = (): JSX.Element =>{
     return(
         <header className={styles.root}>
             <div className="layout-grid">
                 <Logo/>
+                <AuthElement/>
             </div>
         </header>
     );
